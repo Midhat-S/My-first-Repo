3c5e3041cbafff1b121ea8cb4b2dbb9525d967fc
Midhat-S <midhatsafdar17@gmail.com>
2025-05-30 11:45:18 +0000

    Added info about the weather

--- Hello.py
+++
@@ -1,6 +0,0 @@
-a = 1
-b = 2
-c = a + b
-print (c)
-for i in range(1, 11):
-    print(i)
---
+++ Midhat.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-4.1",
+    tools=[{"type": "web_search_preview"}],
+    input="What is the current weather of London?"
+)
+
+print(response.output_text)
