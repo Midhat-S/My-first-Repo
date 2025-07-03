fffab73a1f4de863b62ed4884b1412b4d9e899e4
Midhat-S <midhatsafdar17@gmail.com>
2025-07-03 11:17:15 +0000

    Added new code

--- Forloop.py
+++ Forloop.py
@@ -6,3 +6,28 @@ for color in colors:
     print(color)
     for i in color:
         print(i)
+
+
+for i in range(2, 11, 2):
+    print(i)
+
+word = "hello"
+for letter in word:
+    print(letter)
+
+total = 0
+for i in range(1, 6):
+  total += i
+  print("Sum:", total)
+
+for i in range(3):
+    print("Hello")
+
+for i in range(5):
+    print("Midhat")
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+    for i in color:
+        print(i)
